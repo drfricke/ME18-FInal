661480891bd954bb85f6b4b0c351288cedd76ccd
lalbanese <60711012+lalbanese@users.noreply.github.com>
2020-04-22 15:34:43 -0400

    Update InverseKinematicModeling.m

--- InverseKinematicModeling.m
+++ InverseKinematicModeling.m
@@ -4,7 +4,6 @@ clear
 l1 = .3301; %meters
 l2 = .3429; %meters
  
-
 filelist=dir('*.csv'); %collects all csv in active directory
 %csv naming convention
 %Subject's speed:Shoulder starting angle:Elbow Starting angle:Trial number
@@ -15,34 +14,31 @@ filelist=dir('*.csv'); %collects all csv in active directory
 %First Trial collected with above conditions
 
 for filenow=1:length(filelist) %number of files
-data=load(filelist(filenow).name); %runs through each file
+    data=load(filelist(filenow).name); %runs through each file
 
-thetaElb = data(:,2); %degrees
-thetaSh  = data(:,1); %degrees
-time     = data(:,4); %seconds 
+    thetaElb = data(:,2); %degrees
+    thetaSh  = data(:,1); %degrees
+    time     = data(:,4); %seconds 
 
-%converts to real coordinates
-xHand = l1 * cosd(thetaSh) + l2 * cosd((180-thetaElb)+thetaSh);
-yHand = l1 * sind(thetaSh) + l2 * sind((180-thetaElb)+thetaSh);
+    %Convert to real coordinates
+    xHand = l1 * cosd(thetaSh) + l2 * cosd((180-thetaElb)+thetaSh);
+    yHand = l1 * sind(thetaSh) + l2 * sind((180-thetaElb)+thetaSh);
 
-figure(filenow);
+    %Plot
+    figure(filenow);
 
-subplot(3,1,1);
-plot(time,thetaElb);
-title(strcat("Elbow Degrees vs Time - file",filelist(filenow).name))
-xlabel('Time (ms)') 
-ylabel('Degrees')
+    subplot(2,1,1);
+    hold on
+    plot(time,thetaElb);
+    title(strcat("Elbow Degrees vs Time - file",filelist(filenow).name))
+    xlabel('Time (ms)') 
+    ylabel('Degrees')
+    hold off
 
-subplot(3,1,2);
-plot(time,thetaSh);
-title(strcat("Shoulder Degrees vs Time - file",filelist(filenow).name))
-xlabel('Time (ms)') 
-ylabel('Degrees')
-
-subplot(3,1,3);
-plot(xHand,yHand);
-strcat("Recreation of drawing from data - file",filelist(filenow).name)
-xlabel('Postion (m)') 
-ylabel('Postion (m)')
-
+    subplot(2,1,2);
+    hold on
+    plot(time,thetaSh);
+    title(strcat("Shoulder Degrees vs Time - file",filelist(filenow).name))
+    xlabel('Time (ms)') 
+    ylabel('Degrees')
 end
